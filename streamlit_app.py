7c49b5d71ea00ec6c715985f61144853700c5028
RaziehAkbari2020 <142252168+RaziehAkbari2020@users.noreply.github.com>
2023-08-16 15:00:12 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ def main():
             with open(f'{storename}.pkl',"rb") as f:
                 Vectorstore=pickle.load(f)
         else:
-            embedding=OpenAIEmbeddings(openai_api_key="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")
+            embedding=OpenAIEmbeddings(openai_api_key="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")
             Vectorstore=FAISS.from_texts(chunks,embedding=embedding)
             with open(f'{storename}.pkl',"wb") as f:
                 pickle.dump(Vectorstore,f)
@@ -47,7 +47,7 @@ def main():
 
         if query:
             docs=Vectorstore.similarity_search(query=query, k=3)
-            llm=OpenAI(openai_api_key="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")
+            llm=OpenAI(openai_api_key="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")
             chain=load_qa_chain(llm=llm,chain_type='stuff')
             with get_openai_callback() as cb:
                 response=chain.run(input_documents=docs,question=query)
